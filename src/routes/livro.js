6a010ec7a0e2fe470c1a89ea8880576dff034a18
Luana <santos.luanabeatriz1995@gmail.com>
2023-07-23 21:42:21 +0100

    Criada rota para atualização de um livro

--- src/routes/livro.js
+++ src/routes/livro.js
@@ -20,4 +20,30 @@ router.post('/criar', conectarBancoDados, async function(req, res, next) {
     }
 });
 
+router.put('/editar/:id', conectarBancoDados, async function(req, res, next) {
+    try{
+        // #swagger.tags = ['Livro']
+        let idLivro = req.params.id;
+        let {titulo, paginas, isbn, editora} = req.body;
+        const checkLivro = await EsquemaLivro.findOne({ _id: idLivro});
+        if(!checkLivro) {
+            throw new Error("Livro não encontrado.");
+        }
+
+        const livroAtualizado = await EsquemaLivro.updateOne({_id: idLivro}, {titulo, paginas, isbn, editora});
+
+        if(livroAtualizado.modifiedCount > 0){
+            const dadosTarefa = await EsquemaLivro.findOne({_id: idLivro});
+            
+            res.status(200).json({
+                status: "OK",
+                statusMensagem: "Livro atualizado com sucesso.",
+                resposta: dadosTarefa
+            });
+        }
+    }catch(error){
+        return tratarErrosEsperados(res, error);
+    }
+});
+
 module.exports = router;
