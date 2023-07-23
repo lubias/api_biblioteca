94832bad996923670a1977d40d34a37a79a6f0e6
Luana <santos.luanabeatriz1995@gmail.com>
2023-07-23 21:16:30 +0100

    Criada rota para criar um novo livro

--- src/routes/livro.js
+++ src/routes/livro.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const conectarBancoDados = require('../middlewares/conectarBD');
+const tratarErrosEsperados = require('../functions/tratarErrosEsperados');
+const EsquemaLivro = require('../models/livro');
 
-router.get('/', conectarBancoDados, function(req, res, next) {
-    res.send('respond with a resource 50.');
+router.post('/criar', conectarBancoDados, async function(req, res, next) {
+    try{
+        // #swagger.tags = ['Livro']
+        let {titulo, paginas, isbn, editora} = req.body;
+        const respostaBD = await EsquemaLivro.create({titulo, paginas, isbn, editora});
+
+        res.status(200).json({
+            status: "OK",
+            statusMensagem: "Livro criado com sucesso.",
+            resposta: respostaBD
+        });
+    }catch(error){
+        return tratarErrosEsperados(res, error);
+    }
 });
 
 module.exports = router;
